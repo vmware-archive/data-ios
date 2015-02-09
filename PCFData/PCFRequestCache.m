8025edec1aae5650f62d84ae492b6a941c1062fe
Devin Fallak and Joshua Winters <jwinters@pivotal.io>
2015-02-09 15:01:09 -0500

    Updates the pending request queue priority

--- PCFData/PCFRequestCache.m
+++ PCFData/PCFRequestCache.m
@@ -64,7 +64,7 @@
 }
 
 - (void)executePendingRequests {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         [self executePendingRequestsWithCompletionHandler:nil];
     });
 }
