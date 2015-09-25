a01f99ccd3a218ad4e6382f34c8e7301f490088d
Topher Bullock <cbullock@pivotal.io>
2015-09-25 12:04:05 -0400

    Add os version param to run tests script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-xcodebuild -destination platform='iOS Simulator',name="${XCODE_SIMULATOR_NAME}" -scheme PCFDataTests clean build test
+xcodebuild -destination platform='iOS Simulator',name="${XCODE_SIMULATOR_NAME}",OS="${XCODE_OS_VERSION}" -scheme PCFDataTests clean build test
